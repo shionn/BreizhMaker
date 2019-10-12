2bad322095dd436117662845590375bbd9304e50
Shionn <shionn@gmail.com>
2019-10-12 20:54:50 +0200

    Start BreizhMaker

---
+++ src/main/java/shionn/bm/home/HomeController.java
@@ -0,0 +1,24 @@
+package shionn.bm.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Code sous licence GPLv3 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author <b>Shionn</b>, shionn@gmail.com <i>http://shionn.org</i><br>
+ *         GCS d- s+:+ a+ C++ UL/M P L+ E--- W++ N K- w-- M+ t+ 5 X R+ !tv b+ D+ G- e+++ h+ r- y+
+ */
+@Controller()
+@RequestScope
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+		return new ModelAndView("home");
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%@ page pageEncoding="UTF-8"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<link rel="stylesheet" href="<spring:url value="/css/generated/style.css"/>" />
+<title>BreizhMaker</title>
+</head>
+<body>
+	<script type="text/javascript">var wowhead_searchbox_format = "160x200"</script>
+	<script type="text/javascript" src="http://wow.zamimg.com/widgets/searchbox.js"></script>
+</body>
+</html>
